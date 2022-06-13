dc69e53ccaf6afa92a95b733823ad11b4f525483
anne <messnerbeatrix@gmail.com>
2022-06-13 13:14:52 +0300

    Tema 3 - test

--- Account_factory.cpp
+++ Account_factory.cpp
@@ -3,9 +3,6 @@
 //
 
 #include "Account_factory.h"
-#include "Photo_builder.h"
-#include "Live_builder.h"
-#include "Checkin_builder.h"
 
 std::shared_ptr<Account> Account_factory::create_fast_account(const std::string &username) {
     return std::make_shared<Account>(username, 0, 0, "");
@@ -16,44 +13,14 @@ std::shared_ptr<Account> Account_factory::create_kid_account(const std::string &
 }
 
 std::shared_ptr<Account> Account_factory::create_crypto_influencer(const std::string &username) {
-    Photo_builder pb;
-    Photo p = pb
-            .likes(13242352)
-            .comments(124124)
-            .hashtags({"crypto", "btc"})
-            .theme("crypto")
-            .file_path("sample_btc.jpg")
-            .build();
-    std::shared_ptr<Account> a = std::make_shared<Account>(username, 250000, 10, "crypto");
-    a->add_photo(p);
-    return a;
+    return std::make_shared<Account>(username, 250000, 10, "crypto");
 }
 
 std::shared_ptr<Account> Account_factory::create_fashion_influencer(const std::string &username) {
-    Live_builder lb;
-    Live l = lb
-            .theme("fashion")
-            .people_watching(210000)
-            .starting_time(18)
-            .ending_time(21)
-            .build();
-    std::shared_ptr<Account> a = std::make_shared<Account>(username, 250000, 12, "fashion");
-    a->add_live(l);
-    return a;
+    return std::make_shared<Account>(username, 250000, 12, "fashion");
 }
 
 std::shared_ptr<Account> Account_factory::create_travel_influencer(const std::string &username) {
-    Checkin_builder cb;
-    Check_in c = cb
-            .likes(159341)
-            .hashtags({"dubai", "travel", "magicplace"})
-            .theme("travel")
-            .adress("Dubai Marina")
-            .city("Dubai")
-            .country("UAE")
-            .build();
-    std::shared_ptr<Account> a = std::make_shared<Account>(username, 213214, 2134, "travel");
-    a->add_check_in(c);
-    return a;
+    return std::make_shared<Account>(username, 213214, 2134, "travel");
 }
 
