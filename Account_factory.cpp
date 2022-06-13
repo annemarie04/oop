504b52df41afe2a37cdaa6d099657cd89ae8dc56
anne <messnerbeatrix@gmail.com>
2022-06-13 13:52:50 +0300

    Tema 3 - test

--- Account_factory.cpp
+++ Account_factory.cpp
@@ -5,19 +5,19 @@
 #include "Account_factory.h"
 
 std::shared_ptr<Account> Account_factory::create_fast_account(const std::string &username) {
-    return std::make_shared<Account>(username, 1, 1, "none");
+    return std::make_shared<Account>(Account(username, 1, 1, "none"));
 }
 
 std::shared_ptr<Account> Account_factory::create_kid_account(const std::string &username) {
-    return std::make_shared<Account>(username, 100, 100, "children");
+    return std::make_shared<Account>(Account(username, 100, 100, "children"));
 }
 
 std::shared_ptr<Account> Account_factory::create_fashion_influencer(const std::string &username) {
-    return std::make_shared<Account>(username, 250000, 12, "fashion");
+    return std::make_shared<Account>(Account(username, 250000, 12, "fashion"));
 }
 
 std::shared_ptr<Account> Account_factory::create_travel_influencer(const std::string &username) {
-    return std::make_shared<Account>(username, 213214, 2134, "travel");
+    return std::make_shared<Account>(Account(username, 213214, 2134, "travel"));
 }
 
 Account_factory::~Account_factory() = default;
